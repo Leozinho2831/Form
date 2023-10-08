41a35de2c6f05bbb050f0192fec76636bd34920d
Leozinho2831 <leoferraz1818@gmail.com>
2023-10-08 20:34:58 -0300

    commit: functions in javascript to animations

--- Assets/Javascript/TranslateAnimation.js
+++ Assets/Javascript/TranslateAnimation.js
@@ -1,4 +1,33 @@
-// item.classList.remove('sing'); 
-// item.style.animation = 'translate-reverse 1s forwards';
+function animationCard(){
+    const card = document.querySelector('.form__capa');
+    const titleCard = document.querySelector('.form__capa h1');
 
-// screenWidth = window.innerWidth;
+    if(card){
+        if(card.classList.contains('sing')){
+            card.classList.remove('sing');
+            card.classList.add('sing__out');
+            titleCard.innerHTML = 'Sing-up';
+            
+        } else {
+            card.classList.add('sing');
+            card.classList.remove('sing__out');
+            titleCard.innerHTML = 'Login';
+        }
+    }
+}
+
+function visibilityInput(){
+    const passwordInput = document.querySelectorAll('.form__password');
+
+    if(passwordInput){
+        passwordInput.forEach(function(input){
+            input.classList.toggle('visibility');
+
+            if(input.classList.contains('visibility')) {
+                input.type = 'password';
+            } else {
+                input.type = 'text';
+            }
+        });
+    }
+}
